bc26afb4a5aee0f44d811025c559879c556c751d
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2025-02-12 18:21:26 +0200

    app.py muutoksia julkaisua varten

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask 
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__) # luodaan Flask  sovellus
 CORS(app) # lisätään CORS sovellukseen
@@ -10,15 +11,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # estää turhat SQL kysely
 
 db = SQLAlchemy(app) # luodaan SQLAlchemy tietokanta
 
+frontend_folder = os.path.join(os.getcwd(), '..', 'frontend') # määritetään frontend kansio
+dist_folder = os.path.join(frontend_folder, 'dist') # määritetään dist kansio
 
-#import routes # tuodaan routes.py tiedosto
-
-#with app.app_context():
-    #db.create_all() # luodaan tietokantataulut
-
-#if __name__ == "__main__":
-    #with app.app_context():
-        #db.create_all()
-
-    #app.run(debug=True) # käynnistetään sovellus debug tilassa
-    #app.run(debug=True) # käynnistetään sovellus debug tilassa
+@app.route('/', defaults={'filename': ''}) # määritetään juuri polku
+@app.route('/<path:filename>') # määritetään polku
+def index(filename): # luodaan index funktio
+    if not filename:
+        filename = 'index.html' # määritetään index.html
+    return send_from_directory(dist_folder, filename) # palautetaan tiedosto
