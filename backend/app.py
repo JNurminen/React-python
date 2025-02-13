7bd5672903f86650ba96bf484b278ed1b5a99c9a
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2025-02-13 10:38:30 +0200

    backend muokkaus

--- backend/app.py
+++ backend/app.py
@@ -4,27 +4,28 @@ from flask_cors import CORS
 import os
 
 app = Flask(__name__) # luodaan Flask  sovellus
-CORS(app) # lisätään CORS sovellukseen
+#CORS(app) # lisätään CORS sovellukseen
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db' # luo tietokannan friends.db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # estää turhat SQL kyselyt
 
 db = SQLAlchemy(app) # luodaan SQLAlchemy tietokanta
 
-frontend_folder = os.path.join(os.getcwd(), '..', 'frontend') # määritetään frontend kansio
-dist_folder = os.path.join(frontend_folder, 'dist') # määritetään dist kansio
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
 
-@app.route('/', defaults={'filename': ''}) # määritetään juuri polku
-@app.route('/<path:filename>') # määritetään polku
-def index(filename): # luodaan index funktio
-    if not filename:
-        filename = 'index.html' # määritetään index.html
-    return send_from_directory(dist_folder, filename) # palautetaan tiedosto
+# Server static files from the "dist" folder under the "frontend" directory
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
 
-import routes
+import routes # tuodaan routes.py tiedosto
+
+with app.app_context():
+  db.create_all()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-    app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(debug=True) # käynnistetään sovellus debug tilassa
