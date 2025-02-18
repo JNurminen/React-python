a4207cf61939005a841d0f9cb6baddf54ad1c47f
Jani Nurminen2 <jani.nurminen2@student.careeria.fi>
2025-02-18 09:30:23 +0200

    virheen korjausta

--- backend/app.py
+++ backend/app.py
@@ -24,8 +24,8 @@ def index(filename):
 
 import routes
 
-with app.app_context():
-  db.create_all()
+#if __name__ == "__main__":
+    #with app.app_context():
+        #db.create_all()
 
-if __name__ == "__main__":
-  app.run(debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
